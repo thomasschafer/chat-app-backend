15958ccdfad47530149ac59cf3bc75694a67d543
thomasschafer <thomasschafer97@gmail.com>
2021-11-26 18:31:45 +0000

    Updated allowed origins to include kobble.io domain

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const PORT = 8000;
-export const ALLOWED_ORIGINS = ["http://localhost:3000"];
+export const ALLOWED_ORIGINS = [
+  "http://localhost:3000",
+  "https://kobble.io/",
+  "https://www.kobble.io/",
+];
 export const DB_URI = "mongodb://localhost:27017";
