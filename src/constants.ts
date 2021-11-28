a0c1cf2318f3cf4608d48fd8d77e9f38220b204d
thomasschafer <thomasschafer97@gmail.com>
2021-11-28 20:08:12 +0000

    Adding kobble-api.com to allowed origins

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,7 @@ export const ALLOWED_ORIGINS = [
   "http://localhost:3000",
   "https://kobble.io/",
   "https://www.kobble.io/",
+  "https://kobble-api.com/",
+  "https://www.kobble-api.com/",
 ];
 export const DB_URI = "mongodb://localhost:27017";
