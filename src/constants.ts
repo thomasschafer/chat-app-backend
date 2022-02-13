57469a5ffaed91a21e0d6100850d090697a1049f
thomasschafer <thomasschafer97@gmail.com>
2022-02-13 16:34:33 +0000

    Updated allowed origins

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,5 @@
 export const PORT = 8080;
-export const ALLOWED_ORIGINS = [
-  "http://localhost:3000",
-  "https://kobble.io",
-  "https://www.kobble.io",
-  "https://kobble-api.com",
-  "https://www.kobble-api.com",
-];
+export const ALLOWED_ORIGINS = process.env.CHAT_APP_DEBUG
+  ? ["http://localhost:3000"]
+  : ["https://kobble.io", "https://www.kobble.io"];
 export const DB_URI = "mongodb://mongo:27017";
